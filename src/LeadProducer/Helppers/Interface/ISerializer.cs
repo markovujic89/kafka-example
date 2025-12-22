5825ce1c59ac8ec1a65e0ec561b32e46e7de5a0a
marko.vujic <marko.vujic@propertycaptain.ch>
2025-12-22 07:44:11 +0100

    add schema registry in docker compose

---
+++ src/LeadProducer/Helppers/Interface/ISerializer.cs
@@ -0,0 +1,8 @@
+﻿using Confluent.Kafka;
+
+namespace LeadProducer.Helppers.Interface;
+
+public interface ISerializer<T>
+{
+    byte[] Serialize(T data, SerializationContext context);
+}
---
+++ src/LeadProducer/Helppers/JsonSerializer.cs
@@ -0,0 +1,11 @@
+﻿using Confluent.Kafka;
+
+namespace LeadProducer.Helppers;
+
+public class JsonSerializer<T> : Interface.ISerializer<T>
+{
+    public byte[] Serialize(T data, SerializationContext context)
+    {
+        return System.Text.Json.JsonSerializer.SerializeToUtf8Bytes(data);
+    }
+}
